eddd6ea034688f683d782b1d32ac003554a8328e
Maddie <tmaddy53@gmail.com>
2025-04-29 17:42:04 +0800

    feat: add API gateway configuration and lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,99 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { defineBackend } from "@aws-amplify/backend";
+import { Stack } from "aws-cdk-lib";
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  Cors,
+  LambdaIntegration,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { getStudents } from './functions/get-students/resource.js';
 
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  getStudents,
+});
+
+const apiStack = backend.createStack("api-stack");
+
+const myRestApi = new RestApi(apiStack, "RestApi", {
+  restApiName: "myRestApi",
+  deploy: true,
+  deployOptions: {
+    stageName: "dev",
+  },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_ORIGINS, // Restrict this to domains you trust
+    allowMethods: Cors.ALL_METHODS, // Specify only the methods you need to allow
+    allowHeaders: Cors.DEFAULT_HEADERS, // Specify only the headers you need to allow
+  },
+});
+
+const lambdaIntegration = new LambdaIntegration(
+  backend.myApiFunction.resources.lambda
+);
+
+const itemsPath = myRestApi.root.addResource("items", {
+  defaultMethodOptions: {
+    authorizationType: AuthorizationType.IAM,
+  },
+});
+
+itemsPath.addMethod("GET", lambdaIntegration);
+itemsPath.addMethod("POST", lambdaIntegration);
+itemsPath.addMethod("DELETE", lambdaIntegration);
+itemsPath.addMethod("PUT", lambdaIntegration);
+
+itemsPath.addProxy({
+  anyMethod: true,
+  defaultIntegration: lambdaIntegration,
+});
+
+const cognitoAuth = new CognitoUserPoolsAuthorizer(apiStack, "CognitoAuth", {
+  cognitoUserPools: [backend.auth.resources.userPool],
+});
+
+const booksPath = myRestApi.root.addResource("cognito-auth-path");
+booksPath.addMethod("GET", lambdaIntegration, {
+  authorizationType: AuthorizationType.COGNITO,
+  authorizer: cognitoAuth,
+});
+
+const apiRestPolicy = new Policy(apiStack, "RestApiPolicy", {
+  statements: [
+    new PolicyStatement({
+      actions: ["execute-api:Invoke"],
+      resources: [
+        `${myRestApi.arnForExecuteApi("*", "/items", "dev")}`,
+        `${myRestApi.arnForExecuteApi("*", "/items/*", "dev")}`,
+        `${myRestApi.arnForExecuteApi("*", "/cognito-auth-path", "dev")}`,
+      ],
+    }),
+  ],
+});
+
+// attach the policy to the authenticated and unauthenticated IAM roles
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
+  apiRestPolicy
+);
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
+  apiRestPolicy
+);
+
+// add outputs to the configuration file
+backend.addOutput({
+  custom: {
+    API: {
+      [myRestApi.restApiName]: {
+        endpoint: myRestApi.url,
+        region: Stack.of(myRestApi).region,
+        apiName: myRestApi.restApiName,
+      },
+    },
+  },
 });
--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,31 @@ import AuthWrapper from "@/components/AuthWrapper";
 import { Amplify } from 'aws-amplify';
 import { StudentTable } from "@/components/StudentTable";
 import { NavBar } from "@/components/NavBar";
-
+import { parseAmplifyConfig } from "aws-amplify/utils";
 
 
 Amplify.configure(outputs);
 
+const amplifyConfig = parseAmplifyConfig(outputs);
 
+Amplify.configure(
+  {
+    ...amplifyConfig,
+    API: {
+      ...amplifyConfig.API,
+      REST: outputs.custom.API,
+    },
+  },
+  {
+    API: {
+      REST: {
+        retryStrategy: {
+          strategy: 'no-retry' // Overrides default retry strategy
+        },
+      }
+    }
+  }
+);
 
 export default function App() {
   return (
