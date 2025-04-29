e8a0a29fca8eea07617d7bf558a527d408206f2f
Maddie <tmaddy53@gmail.com>
2025-04-29 12:42:16 +0800

    feat: Add authenticator wrapper component

---
+++ components/AuthWrapper.tsx
@@ -0,0 +1,14 @@
+'use client'; 
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+export default function AuthWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <Authenticator.Provider>
+      <Authenticator>
+        {children}
+      </Authenticator>
+    </Authenticator.Provider>
+  );
+}
